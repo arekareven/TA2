2c4366713ceeb31cfd06b38e0082a7fd3a5bdb65
arekareven <muhammadhasan.emha@gmail.com>
2023-05-25 12:20:38 +0700

    gmaps with data LatLng static, done view background jadi hitam, problem

--- app/src/main/java/com/adityaanugrah/skripsiku/activity/DetailActivity.kt
+++ app/src/main/java/com/adityaanugrah/skripsiku/activity/DetailActivity.kt
@@ -4,23 +4,40 @@ package com.adityaanugrah.skripsiku.activity
 
 import androidx.activity.ComponentActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.adityaanugrah.skripsiku.R
+import com.adityaanugrah.skripsiku.databinding.ActivityDetailBinding
 import com.bumptech.glide.Glide
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_detail.nalamatdetail
 import kotlinx.android.synthetic.main.activity_detail.ndeskrispsidetail
 import kotlinx.android.synthetic.main.activity_detail.ngambar
 /*import kotlinx.android.synthetic.main.activity_detail.nnamaFasilitasdetail*/
 import kotlinx.android.synthetic.main.activity_detail.nnamaWisatadetail
 
-class DetailActivity : ComponentActivity() {
+class DetailActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap : GoogleMap
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+//        setContentView(R.layout.activity_detail)
         nnamaWisatadetail.text = intent.getStringExtra("intent_nnamaWisata")
         nalamatdetail.text = intent.getStringExtra("intent_nalamat")
         ndeskrispsidetail.text = intent.getStringExtra("intent_ndeskripsi")
-
-
 
         Glide.with(this)
             .load(intent.getStringExtra("intent_ngambar"))
@@ -28,5 +45,13 @@ class DetailActivity : ComponentActivity() {
             .error(androidx.appcompat.R.drawable.abc_ratingbar_indicator_material)
             .centerCrop()
             .into(ngambar)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val palembang = LatLng(-2.9765658625421154, 104.7694819886803)
+        mMap.addMarker(MarkerOptions().position(palembang).title("ini palembang"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(palembang))
     }
 }
